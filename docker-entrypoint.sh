fc0eb038acbba24b16833208c1a6731ac140e53d
Lazycoder <restyg86@gmail.com>
2026-06-17 18:59:26 +0800

    kjdhfsdf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,11 @@ set -e
 
 PORT="${PORT:-80}"
 
+# Clear view cache so stale vite() output is never served
+rm -rf /var/www/html/storage/cache/views/*
+mkdir -p /var/www/html/storage/cache/views
+chown -R www-data:www-data /var/www/html/storage
+
 printenv | grep -E "^(APP_|DB_|MAIL_|CORS_|VITE_|SESSION_|VIEW_)" > /var/www/html/.env
 
 sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
