7c68fccfc0a8ce14ae8f00dc67117a3de8534dbf
Lazycoder <restyg86@gmail.com>
2026-06-17 12:57:31 +0800

    fix: add docker-entrypoint.sh for dynamic PORT on Render

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+PORT="${PORT:-80}"
+
+sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/g" /etc/apache2/sites-available/*.conf
+
+exec apache2-foreground
