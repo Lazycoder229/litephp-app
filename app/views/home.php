d7ffaec6360eaaadbad8779d45c0d0d509c08fc3
Lazycoder <restyg86@gmail.com>
2026-06-22 10:41:50 +0800

    fixed the view

--- app/views/home.php
+++ app/views/home.php
@@ -7,10 +7,8 @@
 @section('content')
     <div class="min-h-screen flex flex-col items-center justify-center text-center">
         <h1 class="text-5xl font-bold tracking-tight text-gray-900">
-            Welcomessasa to <span class="text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-gray-900">LitePHP</span>
+            Welcome to <span class="text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-gray-900">LitePHP</span>
         </h1>
-        <p class="mt-4 text-gray-500 text-base max-w-md">
-            Yoghfghfghvfcvbdsdsdsdvxcvxcvcvbcvbcvfggfdgdfgfdgdgdf
-        </p>dfdsdfsdfsdsdhfghffdgfdgdfhffgsdfsdfsdfs
+       
     </div>
 @endsection
